aebd2c4def90ea0ffab6e1cfe72549615a824969
Saskiamei <73471215+Saskiamei@users.noreply.github.com>
2021-01-03 18:35:43 +0000

    Initial commit

---
+++ Game1.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using System.Collections.Generic;
+
+namespace BGINGF
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D playerSheetTxr, platformSheetTxr, whiteBox;
+        SpriteFont uiFont, heartFont;
+        SoundEffect jumpSound, bumpSound, fanfareSound;
+
+        Point screenSize = new Point(800, 450);
+        int levelNumber = 0;
+        PlayerSprite playerSprite;
+        MushroomSprite mushroomSprite;
+
+        List<List<PlatformSprite>> Levels = new List<List<PlatformSprite>>();
+        List<Vector2> mushrooms = new List<Vector2>();
+
+        Scrolling scrolling1;
+        Scrolling scrolling2;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            _graphics.PreferredBackBufferWidth = screenSize.X;
+            _graphics.PreferredBackBufferHeight = screenSize.Y;
+            _graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            playerSheetTxr = Content.Load<Texture2D>("JumpThing_spriteSheet1");
+            platformSheetTxr = Content.Load<Texture2D>("bgingf");
+            uiFont = Content.Load<SpriteFont>("UIFont");
+            heartFont = Content.Load<SpriteFont>("HeartFont");
+            jumpSound = Content.Load<SoundEffect>("jump");
+            bumpSound = Content.Load<SoundEffect>("Bump");
+            fanfareSound = Content.Load<SoundEffect>("fanfare");
+
+            whiteBox = new Texture2D(GraphicsDevice, 1, 1);
+            whiteBox.SetData(new[] { Color.White });
+
+            playerSprite = new PlayerSprite(playerSheetTxr, whiteBox, new Vector2(100, 50), jumpSound, bumpSound);
+            mushroomSprite = new MushroomSprite(playerSheetTxr, whiteBox, new Vector2(200, 200));
+            BuildLevels();
+
+            scrolling1 = new Scrolling(Content.Load<Texture2D>("Background 1"), new Rectangle(0, 0, 800, 450));
+            scrolling2 = new Scrolling(Content.Load<Texture2D>("Background2"), new Rectangle(800, 0, 800, 450));
+            
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            playerSprite.Update(gameTime, Levels[levelNumber]);
+            if (playerSprite.spritePos.Y > screenSize.Y + 50)
+            {
+                playerSprite.lives--;
+                if (playerSprite.lives <= 0)
+                {
+                    playerSprite.lives = 3;
+                    levelNumber = 1;
+                }
+                playerSprite.ResetPlayer(new Vector2(50, 50));
+            }
+
+            if (playerSprite.checkCollision(mushroomSprite))
+            {
+                levelNumber++;
+                if (levelNumber >= Levels.Count) levelNumber = 0;
+                mushroomSprite.spritePos = mushrooms[levelNumber];
+                playerSprite.ResetPlayer(new Vector2(100, 50));
+                fanfareSound.Play();
+            }
+
+            // scrolling background
+            if (scrolling1.rectangle.X + scrolling1.texture.Width <= 0)
+                scrolling1.rectangle.X = scrolling2.rectangle.X + scrolling2.texture.Width;
+            if (scrolling2.rectangle.X + scrolling2.texture.Width <= 0)
+                scrolling2.rectangle.X = scrolling1.rectangle.X + scrolling1.texture.Width;
+
+            scrolling1.Update();
+            scrolling2.Update();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            playerSprite.Draw(_spriteBatch, gameTime);
+
+            foreach (PlatformSprite platform in Levels[levelNumber]) platform.Draw(_spriteBatch, gameTime);
+
+            string livesString = "";
+            if (playerSprite.lives == 3) livesString += "bim";
+            else if (playerSprite.lives == 2) livesString += "im";
+            else if (playerSprite.lives == 1) livesString += "m";
+            else livesString = "";
+
+            _spriteBatch.DrawString(heartFont, livesString, new Vector2(15, 10), Color.White);
+
+
+            _spriteBatch.DrawString(uiFont, "Level " + (levelNumber + 1), new Vector2(screenSize.X - 15 - uiFont.MeasureString("level" + levelNumber + 1).X, 5), Color.White);
+
+
+            scrolling1.Draw(_spriteBatch);
+            scrolling2.Draw(_spriteBatch);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+        void BuildLevels()
+        {
+            Levels.Add(new List<PlatformSprite>());
+            Levels[0].Add(new PlatformSprite(platformSheetTxr, whiteBox, new Vector2(100, 300)));
+            Levels[0].Add(new PlatformSprite(platformSheetTxr, whiteBox, new Vector2(250, 300)));
+            mushrooms.Add(new Vector2(200, 200));
+
+            Levels.Add(new List<PlatformSprite>());
+            Levels[1].Add(new PlatformSprite(platformSheetTxr, whiteBox, new Vector2(100, 400)));
+            Levels[1].Add(new PlatformSprite(platformSheetTxr, whiteBox, new Vector2(250, 350)));
+            mushrooms.Add(new Vector2(400, 200));
+        }
+    }
+}
---
+++ Sprite.cs
@@ -0,0 +1,207 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BGINGF
+{
+    class Sprite
+    {
+
+        Texture2D spriteSheet, collisionTexture;
+
+        public Vector2 spritePos, spriteVelocity,
+            spriteOrigin,
+            spriteScale,
+            collisionInsetMin, collisionInsetMax;
+
+        public bool flipped,
+            isDead,
+            isColliding, drawCollision;
+
+        int collPadding = 5;
+        public List<List<Rectangle>> animations;
+        public int currentAnim, currentFrame;
+        public float frameTime, frameCounter;
+
+        public Sprite(Texture2D newSpritesheet, Texture2D newCollisionTexture, Vector2 newLocation)
+        {
+            spriteSheet = newSpritesheet;
+            collisionTexture = newCollisionTexture;
+            spritePos = newLocation;
+
+            isColliding = false;
+            drawCollision = false;
+            isDead = false;
+            flipped = false;
+            spriteOrigin = new Vector2(0f, 0f);
+            collisionInsetMin = new Vector2(0f, 0f);
+            collisionInsetMax = new Vector2(0f, 0f);
+            spriteScale = new Vector2(1f, 1f);
+            currentAnim = 0;
+            currentFrame = 0;
+            frameTime = 0.5f;
+            frameCounter = frameTime;
+
+            animations = new List<List<Rectangle>>();
+            animations.Add(new List<Rectangle>());
+            animations[0].Add(new Rectangle(5, 5, 246, 315));
+        }
+
+        public virtual void Update(GameTime gameTime) { }
+
+        public void Draw(SpriteBatch spriteBatch, GameTime gametime)
+        {
+            if (animations[currentAnim].Count > 1)
+            {
+                frameCounter -= (float)gametime.ElapsedGameTime.TotalSeconds;
+                if (frameCounter <= 0)
+                {
+                    frameCounter = frameTime;
+                    currentFrame++;
+                }
+                if (currentFrame >= animations[currentAnim].Count) currentFrame = 0;
+            }
+
+            SpriteEffects drawEffect;
+            if (flipped) drawEffect = SpriteEffects.FlipHorizontally;
+            else drawEffect = SpriteEffects.None;
+
+
+            spriteBatch.Draw(
+                spriteSheet,
+                getRectangleForDraw(),
+                animations[currentAnim][currentFrame],
+                Color.White,
+                0f,
+                spriteOrigin,
+                drawEffect,
+                1f);
+
+            if (drawCollision) spriteBatch.Draw(
+                collisionTexture,
+                getRectangleForCollision(),
+                Color.Red);
+        }
+
+        public void setAnim(int newAnim)
+        {
+            if (currentAnim != newAnim)
+            {
+                currentAnim = newAnim;
+                currentFrame = 0;
+                frameCounter = frameTime;
+            }
+        }
+
+        public bool checkCollision(Sprite otherSprite)
+        {
+            if (!isColliding || !otherSprite.isColliding) return false;
+            else return getRectangleForCollision().Intersects(otherSprite.getRectangleForCollision());
+        }
+
+        public bool checkCollisionBelow(Sprite otherSprite)
+        {
+            return checkCollision(otherSprite) && getEdgeBottomForCollision() < otherSprite.getEdgeBottomForCollision() && getEdgeBottomForCollision() > otherSprite.getEdgeTopForCollision()
+                && (getEdgeLeftForCollision() + collPadding < otherSprite.getEdgeRightForCollision() && getEdgeRightForCollision() - collPadding > otherSprite.getEdgeLeftForCollision());
+        }
+
+        public bool checkCollisionAbove(Sprite otherSprite)
+        {
+            return checkCollision(otherSprite) && getEdgeTopForCollision() > otherSprite.getEdgeTopForCollision() && getEdgeTopForCollision() < otherSprite.getEdgeBottomForCollision()
+                && (getEdgeLeftForCollision() + collPadding < otherSprite.getEdgeRightForCollision() && getEdgeRightForCollision() - collPadding > otherSprite.getEdgeLeftForCollision());
+        }
+
+        public bool checkCollisionLeft(Sprite otherSprite)
+        {
+            return checkCollision(otherSprite) && getEdgeRightForCollision() < otherSprite.getEdgeRightForCollision() && getEdgeRightForCollision() > otherSprite.getEdgeLeftForCollision()
+                && (getEdgeTopForCollision() + collPadding < otherSprite.getEdgeBottomForCollision() && getEdgeBottomForCollision() - collPadding > otherSprite.getEdgeTopForCollision());
+        }
+
+        public bool checkCollisionRight(Sprite otherSprite)
+        {
+            return checkCollision(otherSprite) && getEdgeLeftForCollision() > otherSprite.getEdgeLeftForCollision() && getEdgeLeftForCollision() < otherSprite.getEdgeRightForCollision()
+                && (getEdgeTopForCollision() + collPadding < otherSprite.getEdgeBottomForCollision() && getEdgeBottomForCollision() - collPadding > otherSprite.getEdgeTopForCollision());
+        }
+
+        public Rectangle getRectangleForDraw()
+        {
+            return new Rectangle(getEdgeLeftForDraw(), getEdgeTopForDraw(), getWidthForDraw(), getHeightForDraw());
+        }
+
+        public int getWidthForDraw()
+        {
+            return (int)(animations[currentAnim][currentFrame].Width * spriteScale.X);
+        }
+
+        public int getEdgeLeftForDraw()
+        {
+            return (int)(spritePos.X - (getWidthForDraw() * spriteOrigin.X));
+        }
+
+        public int getEdgeRightForDraw()
+        {
+            return getEdgeLeftForDraw() + getWidthForDraw();
+        }
+
+        public int getHeightForDraw()
+        {
+            return (int)(animations[currentAnim][currentFrame].Height * spriteScale.Y);
+        }
+
+        public int getEdgeTopForDraw()
+        {
+            return (int)(spritePos.Y - (getHeightForDraw() * spriteOrigin.Y));
+        }
+
+        public int getEdgeBottomForDraw()
+        {
+            return getEdgeTopForDraw() + getHeightForDraw();
+        }
+
+        public Rectangle getRectangleForCollision()
+        {
+            return new Rectangle(getEdgeLeftForCollision(), getEdgeTopForCollision(), getWidthForCollision(), getHeightForCollision());
+        }
+
+        public int getWidthForCollision()
+        {
+            return getEdgeRightForCollision() - getEdgeLeftForCollision();
+        }
+
+        public int getEdgeLeftForCollision()
+        {
+            return getEdgeLeftForDraw() + (int)(collisionInsetMin.X * getWidthForDraw());
+        }
+
+        public int getEdgeRightForCollision()
+        {
+            return getEdgeRightForDraw() - (int)(collisionInsetMax.X * getWidthForDraw());
+        }
+
+        public int getHeightForCollision()
+        {
+            return getEdgeBottomForCollision() - getEdgeTopForCollision();
+        }
+
+
+        public int getEdgeTopForCollision()
+        {
+            return getEdgeTopForDraw() + (int)(collisionInsetMin.Y * getHeightForDraw());
+        }
+
+
+        public int getEdgeBottomForCollision()
+        {
+            return getEdgeBottomForDraw() - (int)(collisionInsetMax.Y * getHeightForDraw());
+        }
+
+
+        public Vector2 getCentreForCollision()
+        {
+            return new Vector2(getEdgeLeftForCollision() + (getWidthForCollision() * 0.5f), getEdgeTopForCollision() + (getHeightForCollision() * 0.5f));
+        }
+    }
+}
